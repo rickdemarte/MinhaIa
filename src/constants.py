de6d4da14885078b69d62119de0a003820847bb6
Henrique Ribeiro <git@ribeiro.floripa.br>
2025-08-11 18:31:53 -0300

    Adicionado LLM no Groq da openai de 120B

--- src/constants.py
+++ src/constants.py
@@ -6,14 +6,14 @@ DEFAULT_SYSTEM_PROMPT = O_MODEL_SYSTEM_PROMPT
 # PERSONALITY PARA GERAR APENAS O CÓDIGO sem explicações
 CODE_GENERATOR = """Você é um gerador de código. Sua tarefa é fornecer apenas o código necessário para resolver o problema apresentado, sem explicações ou comentários adicionais. Foque em entregar soluções eficientes e diretas."""
 
-# Configurações de áudio
+# Configurações de áudio OpenAi
 DEFAULT_VOICE = "alloy"
 #DEFAULT_TTS_MODEL = "gpt-4o-audio-preview"
 DEFAULT_TTS_MODEL = "gpt-4o-mini-tts"
 #DEFAULT_TTS_MODEL = "tts-1"
 #DEFAULT_TTS_MODEL = "tts-1-hd"
 DEFAULT_AUDIO_FILE = "voz.mp3"
-VOICE_INSTRUCTIONS = "Você é um professor que fala clara e pausadamente"
+VOICE_INSTRUCTIONS = "Você é um professor no Brasil que fala clara e pausadamente"
 
 # Constantes para AWS Polly
 DEFAULT_VOICE_ID = "Vitoria"  # Voz brasileira feminina
